96cda6f36c36282a3ef187aca62151e2c2df26c1
Kartheek Bhupathiraju <kbhupathiraju-c@g2llc.com>
2018-10-08 21:13:28 +0530

    Added Params class

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,6 +1,9 @@
-class ssh::service {
-	service { 'sshd':
+class ssh::service (
+		String $service_name = $::ssh::service_name,
+	) {
+	service { 'ssh-service':
 		enable      => true,
+		name        => $service_name, 
 		ensure      => running,
 		hasrestart => true,
 		hasstatus  => true,
